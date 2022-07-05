d4a7e2cd8acbbaeb9cc6044d89d0fb312f2496df
mzhans <98946477+mzhans@users.noreply.github.com>
2022-07-05 22:28:57 +0700

    Commit tugas-4

---
+++ tugas-4/src/App.js
@@ -0,0 +1,13 @@
+import Tombol from "./Tombol";
+
+function App(){
+    return (
+        <div>
+            <Tombol name=" Aren "/>
+            <Tombol name="Kurnia"/>
+            <Tombol name="Cantik"/>
+        </div>
+    )
+}
+
+export default App;
